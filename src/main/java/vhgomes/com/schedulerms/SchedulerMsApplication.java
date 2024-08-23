ce9625254ce0b0d31f9e2959c14d6aa6a0e2b57a
Victor Hugo <vhclaudiogomes@gmail.com>
2024-08-23 11:59:24 -0300

    initial commit

---
+++ src/main/java/vhgomes/com/schedulerms/SchedulerMsApplication.java
@@ -0,0 +1,13 @@
+package vhgomes.com.schedulerms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vhgomes/com/schedulerms/SchedulerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package vhgomes.com.schedulerms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
